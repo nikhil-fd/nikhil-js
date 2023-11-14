7a7090e19119e5581adbd1dd670a14d5454e9570
nikhil-fd <nikhildas.fd@gmail.com>
2023-11-14 10:09:26 +0530

    add datatype

---
+++ 01_variable.js
@@ -0,0 +1,6 @@
+var accountId = 6032;
+let accountName = "nikhil6032";
+const accountPwd = "Happy@1222";
+// accountPwd = "New@2024";
+// console.log(accountPwd);
+console.table([accountId, accountName, accountPwd]);
---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+let myFavPlayer = "virat kohli";
+let playerScore = 78;
+let isPlayerRetire = false;
+let playerAge;
+let playerCentury = null;
+let jerseyNo = "18";
+let seriesCentury = 1;
+
+
+// console.table([typeof(myFavPlayer),typeof(playerScore), typeof(isPlayerRetire), typeof(playerAge), typeof(playerCentury)]);
+// console.table([isNaN(myFavPlayer), isNaN(playerScore), isNaN(isPlayerRetire), isNaN(playerAge), isNaN(playerCentury)]);
+// console.log(Number(jerseyNo));
+// console.log(jerseyNo);
+
+//Here we convert the value from string to number using Number() method.
+// console.log(typeof(Number(jerseyNo)));
+// console.log(typeof(jerseyNo));
+
+//Here we convert the Number into Boolean using Boolean() method.
+console.log(Boolean(seriesCentury));
+console.log(typeof(Boolean(seriesCentury)));
+
+//Here we convert the number into string using Stirng() method.
+console.log(String(playerScore));
+console.log(typeof(String(playerScore)));
