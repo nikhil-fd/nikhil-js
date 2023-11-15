e18ea7718c18aea8213741cf734d66d28cd52f6e
nikhil-fd <nikhildas.fd@gmail.com>
2023-11-15 11:13:47 +0530

    explained all arrary methods

--- 06_array.js
+++ 06_array.js
@@ -10,13 +10,43 @@ console.log(newArr);
 //          if you are deleteing any element into an array then it returns the deleted element into an array.
 //          if you are not deleteing any element only adding element then it returns empty array.
 const ourPlayers = ["virat","rahul","rohit","iyer"];
-const updatedPlayers = ourPlayers.splice(3,1,"gill");
-console.log(updatedPlayers);
-console.log(ourPlayers);
+const updatedPlayers = ourPlayers.splice(1,3);
+const allPlayers = ourPlayers.splice(3,0,"gill");
+console.log("A ", updatedPlayers);
+console.log("C ", allPlayers);
+console.log("D ", ourPlayers);
 
 //slice(): it returns the extracted element and it starts extracting from start index number
-//         to till before end index no.it doesn't include end argument.
+//         to till before the end index no.it doesn't include end argument.
 let mySkill = ["sap","html","css","js","react js","bootstrap"];
 let sliceValue = mySkill.slice(1,4);
 console.log(sliceValue);
 console.log(mySkill);
+
+//spread operator...
+const myIplBatsman = ["virat","dhoni","ishan","surya","maxwell"];
+const myIplBowlers = ["chahal","shami","siraj","kuldeep"];
+const mergeAllPlayers = [...myIplBatsman,...myIplBowlers];
+console.log(mergeAllPlayers);
+
+//flat():
+const favJesey = [18,7,1,[17,25,100],[65,87,95]];
+const flatArr = favJesey.flat(Infinity);
+console.log(flatArr);
+
+//😍how to make your string value into an array?
+//1st it checks is it array value or not.
+console.log(Array.isArray("NIKHIL"));
+//2nd it converts that string value into an array.
+console.log(Array.from("NIKHIL"));
+
+const myName = "Nihil Das";
+console.log(Array.isArray(myName));
+
+const myTech = "ReactJS Developer";
+console.log(Array.from(myTech));
+
+//it converts all the variable value into an array.
+const myProject1 = "ecommerce";
+const myProject2 = "calculator";
+console.log(Array.of(myProject1,myProject2));
