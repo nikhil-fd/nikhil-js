39d998acd29cbb0393382a5609df398c0859d505
nikhil-fd <nikhildas.fd@gmail.com>
2023-11-19 12:02:41 +0530

    explained about Execution Context

---
+++ 11_execution_context.js
@@ -0,0 +1,25 @@
+//🎯Most Important Topic:👉 Execution Context work flow diagram explaination follow the Note book and video of "Chai aur Code" JS video.
+
+//🚩Execution Context🚩
+//👉Execution context means whatever Code file/Program file we have made to that code file, how js run and how js execute that code file i.e. called Execution Context.
+//👉JS runs that code file in 2 phase-
+//    (1) Memory Creation Phase -- It allocates memory only(when when define variable) 
+//    (2) Execution Phase       -- It performs all arithmatic operation (like +, -, /, *, % etc..)
+
+//👉When js code runs, actually it runs inside an excution context.There are 3 types of code created in a new execution context.
+//    (1) Global Execution Context              (Global execution context created - when variable is defined globally like outside function / if any code that exist outside of the JS function)
+//    (2) Function/Functional Execution Context (Each function runs within its own execution context)
+//    (3) Eval Execution Context                (Optional--used in mongo db --it creates a new execution context)
+
+//👉After created global execution context the it gets store in "this" variable.
+//👉Keep in mind-- Browser -- Global execution context is different (it contains window object)
+//                 node enviornment -- Global execution context is different
+
+
+
+
+
+
+
+
+
