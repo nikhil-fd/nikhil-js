8cb3a7b0242f7169e43d1f1a215d0c05383661da
nikhil-fd <nikhildas.fd@gmail.com>
2023-11-21 22:06:34 +0530

    updated some new features in array

--- 06_array.js
+++ 06_array.js
@@ -1,11 +1,13 @@
-//Array: We can store multiple element in mutliple data types on a single variable i.e. called Array.
-//join(): it adds all the elements of array into a string and separeated by comma bydefault.
+//🚩Array: We can store multiple element in mutliple data types on a single variable i.e. called Array.
+
+//🚩join(): it adds all the elements of array into a string and separeated by comma bydefault.
 //        comma is bydefaut but we can add our own symbol instead of comma like arrow,star etc..
 const myFavPlayers = ["Virat",18,true,"Rahul",1,false,"Rohit",42,true];
 const newArr = myFavPlayers.join("-->");
 console.log(newArr);
 
-//splice(): this method is usually used for adding and removing element into a specific location
+
+//🚩splice(): this method is usually used for adding and removing element into a specific location
 //          into an array.it takes 3 arguments start index,delete count and new element.
 //          if you are deleteing any element into an array then it returns the deleted element into an array.
 //          if you are not deleteing any element only adding element then it returns empty array.
@@ -16,23 +18,27 @@ console.log("A ", updatedPlayers);
 console.log("C ", allPlayers);
 console.log("D ", ourPlayers);
 
-//slice(): it returns the extracted element and it starts extracting from start index number
+
+//🚩slice(): it returns the extracted element and it starts extracting from start index number
 //         to till before the end index no.it doesn't include end argument.
 let mySkill = ["sap","html","css","js","react js","bootstrap"];
 let sliceValue = mySkill.slice(1,4);
 console.log(sliceValue);
 console.log(mySkill);
 
-//spread operator...
+
+//🚩spread operator...
 const myIplBatsman = ["virat","dhoni","ishan","surya","maxwell"];
 const myIplBowlers = ["chahal","shami","siraj","kuldeep"];
 const mergeAllPlayers = [...myIplBatsman,...myIplBowlers];
 console.log(mergeAllPlayers);
 
-//flat():
+
+//🚩flat():
 const favJesey = [18,7,1,[17,25,100],[65,87,95]];
 const flatArr = favJesey.flat(Infinity);
 console.log(flatArr);
+
 
 //😍how to make your string value into an array?
 //1st it checks is it array value or not.
@@ -46,12 +52,19 @@ console.log(Array.isArray(myName));
 const myTech = "ReactJS Developer";
 console.log(Array.from(myTech));
 
-//it converts all the variable value into an array.
+
+//🚩it converts all the variable value into an array.
 const myProject1 = "ecommerce";
 const myProject2 = "calculator";
 console.log(Array.of(myProject1,myProject2));
 
-//Array Destructuring
+
+//🚩Array Destructuring
 const myBio = ["Nikhil", 3, "React Js", "Flipkart"];
 const [name, exp, skill, company] = myBio;
 console.log(skill);
+
+
+//🚩Array.from(data)
+//It converts any type of data into array.
+//check the example in file "dom.html"
