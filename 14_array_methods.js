51528752504800857a431f18d81a1766678f26dc
nikhil-fd <nikhildas.fd@gmail.com>
2023-11-22 19:34:38 +0530

    explained about array methods

--- 14_array_methods.js
+++ 14_array_methods.js
@@ -21,7 +21,7 @@ const searchValue = myNum.filter((elem) => {
 console.log(searchValue);
 
 
-//Same Example1 we can do by forEach() loop also.👇
+//Same Example1 we can do by forEach() loop also..👇
 const myScore = [10,20,30,40,50,60,70,80,90,100];
 const searchScore = [];
 myScore.forEach((elem) => {
