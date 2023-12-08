540f6bbd873d718254ca2d87b5ad42452b158022
nikhil-fd <nikhildas.fd@gmail.com>
2023-12-08 21:42:41 +0530

    explained lexical scoping

---
+++ 31_lexical_scoping.js
@@ -0,0 +1,36 @@
+//🚩Lexical Scoping:
+//Lexical scoping means inner function scope can get access to their parent functions variable,
+//But vice versa not true(that means outer function can not get access inner function variable).
+
+//"var": using "var" keyword vice versa can possible like parent scope function can access inner function variable.
+//"let" & "const" : using "let" & "const" only inner function can access outer function variable but
+//                  outer function can't access inner function variable.
+
+function outerFunc(){
+    let myName = "Nikhil"
+
+    function innerFuncOne(){
+        let mySkill = "ReactJS"
+        console.log("inner", myName);
+    }
+    innerFuncOne();
+
+    function innerFuncTwo(){
+        console.log("inner 2", myName);
+        console.log("inner1", mySkill);
+    }
+    innerFuncTwo();
+
+    console.log(`My Name is ${myName} and my skill is ${mySkill}`);
+}
+outerFunc();
+
+//🌟Note: Here one parent function and that parent function has 2 child function both child function
+//      can access parent function data but both child can not get access another child data
+//      (like func1 can't access fun2 data and func2 can't access fun1 data).
+
+//only execution context khali return hauni ta sahito lexical scope bhi return hauchi.
+//closure returns karuchi pura function(parent function ku) jodi sey parent function
+//bhitore child function bhi achi ta hele b child + parent sabu return hei jibo by closure.
+
+//Interview: closure return karuchi pura funcion sirf function nuha pura lexical scope hine return karuchi.
