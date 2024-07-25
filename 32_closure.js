49ec056843b1c2953035990df443a299ff1fe4f0
nikhil-fd <nikhildas.fd@gmail.com>
2024-07-25 19:27:12 +0530

    explained closure and lexical scope

--- 32_closure.js
+++ 32_closure.js
@@ -3,9 +3,9 @@
 //👉In other words Closure gives access to an outer function scope from an inner function scope.
 //👉It means the inner function can access outer function variable but outer function can't access inner function variable.
 //👉In JS closure are created every time, when function is created.
-//👉Closure uses "return" keyword to return the lexical scope.
+//👉closure returns complete lexical scope, it doesn't return only return statment's executional context.
 
-//👍Interview: closure return karuchi pura funcion sirf function nuha pura lexical scope hine return karuchi.
+//👍Interview: closure return karuchi pura funcion sirf return statement function nuha pura lexical scope hine return karuchi.
 
 
 //Example1:
@@ -18,3 +18,9 @@ function outerFunc(){
 }
 const myFunc = outerFunc();
 myFunc();
+//💥Output: Nikhil
+
+//👌Note: In Example1 we understood that there return statement returning innerFunc(),
+//      but innerFunc() consoleing/executional context is myName but myName is declared in outerFunc(),
+//      so it should be return "error" because myName declared in outerFunc but outerFunc is not returned only,
+//      innerFunc is returned.So here closure returns complete lexical scope(both outerFunc and innerFunc)  
