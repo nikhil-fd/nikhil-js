afe30d3cf0b02e567b3cc2bb106e70a2978ebae3
nikhil-fd <nikhildas.fd@gmail.com>
2024-09-05 18:14:30 +0530

    explained event loop, fetch, execution context

--- 35_event_loop.js
+++ 35_event_loop.js
@@ -11,7 +11,7 @@
 
 //In call stack the tasks are executed 1 by 1, in which order they were added.
 //When call stack is empty then event loop checking the callback queue, 
-//if there are any task in callback queue to moved to the call stack for executiion. 
+//if there are any task in callback queue to moved to the call stack for executiion.. 
 
 //🤷‍♂️For Best Understanding Follow up the Diagram below.👇
 
